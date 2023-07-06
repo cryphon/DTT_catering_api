b432099dcfb0e29e3c7b9fec777de75f9780e04c
Yvan Roes <690101@student.inholland.nl>
2023-07-07 01:31:56 +0200

    added routes corresponding to functionalities for facilities

--- routes/routes.php
+++ routes/routes.php
@@ -5,4 +5,9 @@
 // Define routes here
 $router->get('/test', App\Controllers\IndexController::class . '@test');
 $router->get('/', App\Controllers\IndexController::class . '@test');
-$router->post('/facility', App\Controllers\FacilityController::class. '@create');
+
+$router->post('/facility', App\Controllers\FacilityController::class. '@createNewFacility');
+$router->get('/facility', App\Controllers\FacilityController::class. '@getOneFacility');
+
+
+$router->get('/facilities', App\Controllers\FacilityController::class. '@getAll');
