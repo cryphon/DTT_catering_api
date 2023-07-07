c6c54ee6fccd74c055c3c6b77b6ce93f136aacd9
Yvan Roes <690101@student.inholland.nl>
2023-07-07 02:51:31 +0200

    added CRUD for Facility

--- routes/routes.php
+++ routes/routes.php
@@ -8,6 +8,7 @@ $router->get('/', App\Controllers\IndexController::class . '@test');
 
 $router->post('/facility', App\Controllers\FacilityController::class. '@createNewFacility');
 $router->get('/facility', App\Controllers\FacilityController::class. '@getOneFacility');
-
+$router->put('/facility', App\Controllers\FacilityController::class . '@updateFacility');
+$router->delete('/facility', App\Controllers\FacilityController::class . '@deleteFacility');
 
 $router->get('/facilities', App\Controllers\FacilityController::class. '@getAll');
